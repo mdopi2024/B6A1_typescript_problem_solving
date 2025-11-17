320d30ba9937214d6720e081cd094d591b8a79fc
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2025-11-17 11:32:45 +0600

    problem 6 done

--- solution.ts
+++ solution.ts
@@ -2,26 +2,26 @@
 
 type FormatValueType = string | number | boolean;
 
-const formatValue=<T extends FormatValueType>(value:FormatValueType):FormatValueType=>{
- if(typeof value === 'string'){
-    return value.toUpperCase()
- }else if(typeof value === 'number'){
-    return value*10;
- }else{
-    return !value;
- }
+const formatValue = <T extends FormatValueType>(value: FormatValueType): FormatValueType => {
+    if (typeof value === 'string') {
+        return value.toUpperCase()
+    } else if (typeof value === 'number') {
+        return value * 10;
+    } else {
+        return !value;
+    }
 }
 
-const formatedValue=formatValue('hello world')
+const formatedValue = formatValue('hello world')
 
 // problem 2
 type GetLengthType = string | any[]
-const getLength = (value:GetLengthType) :number =>{
-  if(Array.isArray(value)){
-    return value.length;
-  }else{
-    return value.length;
-  }
+const getLength = (value: GetLengthType): number => {
+    if (Array.isArray(value)) {
+        return value.length;
+    } else {
+        return value.length;
+    }
 }
 
 const gotLength = getLength('my name is opi korim')
@@ -30,16 +30,16 @@ const gotLength = getLength('my name is opi korim')
 //problem 3
 
 class Person {
-    name:string;
-    age:number;
+    name: string;
+    age: number;
 
-    constructor(name:string, age:number){
-        this.name= name;
+    constructor(name: string, age: number) {
+        this.name = name;
         this.age = age
     }
 
-    getDetails (){
-      return ` 'Name: ${this.name}, Age: ${this.age}';`
+    getDetails() {
+        return ` 'Name: ${this.name}, Age: ${this.age}';`
     }
 }
 
@@ -49,30 +49,70 @@ const person2 = new Person('Alice', 25);
 
 //problem 4
 
-type Book={
-    title:string;
-    rating:number;
+type Book = {
+    title: string;
+    rating: number;
 }
 
 
-const filterByRating =<T extends Book>(bookArray:T[]):T[]=>{
- const filtered = bookArray.filter(book=> book.rating >=4);
- return filtered;
+const filterByRating = (bookArray: Book[]): Book[] => {
+    const filtered = bookArray.filter(book => book.rating >= 4);
+    return filtered;
 }
-
 
 const books = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
 ]
 
 const filteredBooks = filterByRating(books)
 
 
+//problem 5
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+const filterActiveUsers = (users: User[]): User[] => {
+    const filterUsers = users.filter(user => user.isActive === true)
+    return filterUsers
+}
+
+const users = [
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true, },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
 
 
+const activeUsers = filterActiveUsers(users)
 
 
+// problem 6
 
+type Book2 = {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+
+}
+const printBookDetails = (book: Book2) => {
+    const { title, author, publishedYear, isAvailable } = book
+    console.log(`Title: ${title}, Author: ${author}, Published: ${publishedYear}, Available: ${isAvailable ? 'Yes' : 'No'}`)
+};
+
+const myBook: Book2 = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    publishedYear: 1925,
+    isAvailable: true,
+};
+
+const printBook = printBookDetails(myBook)
 
