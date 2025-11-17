eb534f1d39574042b8b82d74c1760fa8f3b885f3
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2025-11-17 10:02:43 +0600

    problem 2 done

--- solution.ts
+++ solution.ts
@@ -1,7 +1,8 @@
 // problem 1;
 
 type FormatValueType = string | number | boolean;
-const formatValue=(value:FormatValueType):FormatValueType=>{
+
+const formatValue=<T extends FormatValueType>(value:FormatValueType):FormatValueType=>{
  if(typeof value === 'string'){
     return value.toUpperCase()
  }else if(typeof value === 'number'){
@@ -12,3 +13,20 @@ const formatValue=(value:FormatValueType):FormatValueType=>{
 }
 
 const formatedValue=formatValue('hello world')
+
+// problem 2
+type GetLengthType = string | any[]
+const getLength = (value:GetLengthType) :number =>{
+  if(Array.isArray(value)){
+    return value.length;
+  }else{
+    return value.length;
+  }
+}
+
+const gotLength = getLength('my name is opi korim')
+
+
+//problem 3
+
+
